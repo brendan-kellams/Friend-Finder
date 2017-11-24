5b99b56ded9992fa1f90c0dd837436c57d06b6d3
Brendan Kellams <brendan.kellams@gmail.com>
2017-11-23 18:18:00 -0800

    made server logic

---
+++ server.js
@@ -0,0 +1,26 @@
+// Required Dependencies
+var express = require("express");
+var bodyParser = require('body-parser');
+var path = require('path');
+
+// Config Express
+var app = express();
+var PORT = process.env.PORT;
+
+// Creating a path to allow access to the public file
+app.use(express.static(path.join(__dirname,'./app/public')));
+
+// Parsing data from user
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+
+// Creating routes
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+// Start listening on Port
+app.listen(PORT, function() {
+    console.log('Friend Finder app is listening on port: ' + PORT);
+});
+
