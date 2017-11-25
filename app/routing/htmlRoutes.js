b19c6a1d27550ceea4f3f6a5d693046d26fefb22
Brendan Kellams <brendan.kellams@gmail.com>
2017-11-24 19:44:37 -0800

    worked on the htmlRoutes.js file

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,17 @@
+// Required Dependencies
+var path = require('path');
+
+// Export routes
+module.exports = function(app) {
+
+    // Home Page
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/home.html'));
+    });
+
+    // Survey Page
+    app.get('/survey', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/survey.html'));
+    });
+
+};
