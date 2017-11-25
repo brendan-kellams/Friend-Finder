fa1ba2532b8430e8aa3e89fd96aec85ff5968e95
Brendan Kellams <brendan.kellams@gmail.com>
2017-11-25 08:56:08 -0800

    made a change to server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 
 // Config Express
 var app = express();
-var PORT = process.env.PORT;
+var PORT = process.env.PORT || 3000;
 
 // Creating a path to allow access to the public file
 app.use(express.static(path.join(__dirname,'./app/public')));
