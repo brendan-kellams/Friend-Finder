601e0417e0ad908a1d2d92b5c9fbaaf62e875703
Brendan Kellams <brendan.kellams@gmail.com>
2017-11-25 02:25:19 -0800

    still need to make some major changes

---
+++ app/data/friends.js
@@ -0,0 +1,54 @@
+// Friend Data
+
+var userArray = [
+    {
+        name: 'King Arthur: King of the Britians',
+        photo: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjAweKDn9nXAhVRleAKHS_zB3MQjRwIBw&url=http%3A%2F%2Ftvtropes.org%2Fpmwiki%2Fpmwiki.php%2FCharacters%2FMontyPythonAndTheHolyGrail&psig=AOvVaw2InUcRmDuBRI8IhxHKvGfZ&ust=1511682629060731',
+        scores: [
+            '4',
+            '4',
+            '2',
+            '4',
+            '5',
+            '1',
+            '5',
+            '3',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Honey Boo Boo',
+        photo: 'https://img.huffingtonpost.com/asset/560d2f9a1800002a0083170d.jpeg?ops=scalefit_960_noupscale',
+        scores: [
+            '1',
+            '4',
+            '5',
+            '5',
+            '5',
+            '5',
+            '1',
+            '5',
+            '5',
+            '5'
+        ]
+    },
+    {
+        name: 'The Most Boring Man Alive',
+        photo: 'https://i.ytimg.com/vi/qX4giG15K9A/hqdefault.jpg',
+        scores: [
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1'
+        ]
+    }
+];
+
+module.exports = userArray;
